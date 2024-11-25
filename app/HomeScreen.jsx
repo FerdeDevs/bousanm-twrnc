7e19a00caa71f97bea75bdae6f5d0f6a9a7c8b80
FerdeDevs <pferdelson@gmail.com>
2024-11-25 15:42:27 -0800

    qq

--- app/HomeScreen.jsx
+++ app/HomeScreen.jsx
@@ -8,9 +8,15 @@ const HomeScreen = () => {
             <ImgBackground />
             <ScrollView>
                 <View style={{ paddingHorizontal: 16, paddingVertical: 22 }}>
-                    <Pressable style={{ width: 50, height: 50, borderRadius: 100, backgroundColor: "lightGrey", padding: 4, overflow: "hidden" }}>
-                        <Image source={require('../assets/images/user.png')} style={{ width: "100%", height: "100%", marginTop: 6 }} />
-                    </Pressable>
+                    <View>
+                        <Pressable style={{ width: 50, height: 50, borderRadius: 100, backgroundColor: "lightGrey", padding: 4, overflow: "hidden" }}>
+                            <Image source={require('../assets/images/user.png')} style={{ width: "100%", height: "100%", marginTop: 6 }} />
+                        </Pressable>
+                        <View>
+                            <Text style={{ fontWeight: "bold", fontSize: 18 }}>User Name</Text>
+                            <Text>User Email</Text>
+                        </View>
+                    </View>
 
                     <View>
 
