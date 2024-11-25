319e80435156f902dea735280056792fa2f92023
FerdeDevs <pferdelson@gmail.com>
2024-11-25 14:29:17 -0800

    hey

--- app/HomeScreen.jsx
+++ app/HomeScreen.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView, Pressable } from 'react-native'
 import React from 'react'
+import ImgBackground from './../components/ImgBackground';
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={{ flex: 1, }}>
+            <ImgBackground />
+            <ScrollView>
+                <View style={{ paddingHorizontal: 16, paddingVertical: 22 }}>
+                    <Pressable style={{ width: 50, height: 50, borderRadius: 100, backgroundColor: "lightGrey", padding: 4, overflow: "hidden" }}>
+                        <Image source={require('../assets/images/user.png')} style={{ width: "100%", height: "100%", marginTop: 6 }} />
+                    </Pressable>
+
+                    <View>
+
+                    </View>
+                </View>
+                <Text>HomeScreen fs</Text>
+            </ScrollView>
         </View>
     )
 }
